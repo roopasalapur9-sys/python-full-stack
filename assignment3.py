1c9b9dac923a6948595a081b3a4793a6e092d999
roopasalapur9-sys <roopasalapur9@gmail.com>
2026-01-10 11:15:03 +0530

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,8 @@
+username="roopa"
+password="roop@2005"
+user_name=input("enter username:")
+user_password=input("enter password:")
+if user_name==username and user_password==password:
+    print("login successful")
+else:
+    print("access denied")
