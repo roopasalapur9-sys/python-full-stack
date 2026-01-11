6749094a9201824fa782f2bc23c2ac6836debef3
roopasalapur9-sys <roopasalapur9@gmail.com>
2026-01-11 11:53:01 +0530

    Add files via upload

---
+++ assignment6.py
@@ -0,0 +1,5 @@
+item_prices=[10.99,89.45,6.45,5.67]
+total_order_value=sum(item_prices)
+average_item_price=total_order_value/len(item_prices)
+print("total_order_value",total_order_value)
+print("average_item_price",average_item_price)
